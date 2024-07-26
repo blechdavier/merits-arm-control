13b8a96e22d8e8251eb6f772180db2ee40294fef
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2024-07-26 18:34:43 -0400

    update path in postcreate

--- .devcontainer/postcreate.bash
+++ .devcontainer/postcreate.bash
@@ -1,4 +1,2 @@
-function build() { echo \"Installing dependencies...\";rosdep install --from-paths /workspaces/ros_ws/src --ignore-src -ry;echo \"Calling catkin build...\";catkin build;echo \"Sourcing devel/setup.bash...\";source /workspaces/ros_ws/devel/setup.bash;echo \"Sourced.\"; }
-
 rm -rf build devel logs
-rosdep install --from-paths /workspaces/ros_ws/src --ignore-src -ry
+rosdep install --from-paths /workspaces/merits-arm-control/src --ignore-src -ry
